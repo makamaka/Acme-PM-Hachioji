c6f668f4e13515f78000bedd2c62e47b550e1ba6
makamaka <makamaka.donzoko@gmail.com>
2010-12-24 14:27:54 +0900

    init

---
+++ lib/Acme/PM/Hachioji.pm
@@ -0,0 +1,37 @@
+package Acme::PM::Hachioji;
+
+use strict;
+use warnings;
+
+our $VERSION = '2010.1224';
+
+1;
+__END__
+
+=pod
+
+=head1 NAME
+
+Acme::PM::Hachioji - Hachioji.pm in Japan
+
+=head1 SYNOPSIS
+
+ $ perldoc Acme::PM::Hachioji;
+
+=head1 DESCRIPTION
+
+Hachioji.pmの活動報告とかなんかじゃないのかしら？
+
+=head1 AUTHOR
+
+Makamaka Hannyaharamitu, E<lt>makamaka[at]cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 by Makamaka Hannyaharamitu
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. 
+
+=cut
+
---
+++ lib/Acme/PM/Hachioji/Meetings.pm
@@ -0,0 +1,41 @@
+package Acme::PM::Hachioji::Meetings;
+
+use strict;
+use warnings;
+
+our $VERSION = '2010.1224';
+
+1;
+__END__
+
+=pod
+
+=head1 NAME
+
+Acme::PM::Hachioji::Meetings - Hachioji.pm in Japan
+
+=head1 SYNOPSIS
+
+ use Acme::PM::Hachioji::Meetings;
+ 
+ my $mt = Acme::PM::Hachioji::Meetings->new;
+ 
+ $mt->next_meeting();
+
+=head1 DESCRIPTION
+
+Hachioji.pmの集まりの日程とかじゃないかしら？
+
+=head1 AUTHOR
+
+Makamaka Hannyaharamitu, E<lt>makamaka[at]cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 by Makamaka Hannyaharamitu
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. 
+
+=cut
+
